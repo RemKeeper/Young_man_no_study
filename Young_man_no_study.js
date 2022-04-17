bcad97e7e0090c67a07605d75d3c75e076ed9c0d
RemKeeper <78486275+RemKeeper@users.noreply.github.com>
2022-04-18 01:11:12 +0800

    新增下载截图功能
    点击自动下载完全截图

--- Young_man_no_study.js
+++ Young_man_no_study.js
@@ -8,6 +8,20 @@
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // @run-at document-idle
+// @require      https://code.jquery.com/jquery-3.6.0.min.js
 // ==/UserScript==
 
 document.getElementsByClassName('section3')[0].className = "section3 topindex2"
+console.log('成功跳过视频')
+var url=document.location.href;
+var url_num=url.indexOf('m.html');
+var pic_url = url.slice(0,url_num)+"images/end.jpg";
+var down = document.createElement("a");
+down.style.fontSize="25px";
+down.style.width="448px"
+down.style.height="700px"
+down.style.position="absolute"
+down.href="https://h5.cyol.com/special/daxuexi/ct3fd2g8mp/images/end.jpg";
+down.download="end.jpg";
+document.getElementsByClassName('section3')[0].appendChild(down)
+document.getElementsByClassName('section3')[0].style.background="url(https://s1.328888.xyz/2022/04/18/rgNqy.png)";
